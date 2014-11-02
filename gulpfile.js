525fe934faa7e077e88c6fd1dc14c9ffeecdb5ca
Mike Francis <mikeffrancis@gmail.com>
2014-11-02 12:12:23 +0000

    Amended gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,20 @@
 var gulp         = require('gulp'),
+    sass         = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     pixrem       = require('gulp-pixrem')
     minifycss    = require('gulp-minify-css');
 
 gulp.task('css', function() {
-  gulp.src('css/style.css')
+  gulp.src('css/**/*.scss')
+    .pipe(sass())
     .pipe(autoprefixer('last 10 version'))
-    .pipe(pixrem())
     .pipe(minifycss())
-    .pipe(gulp.dest('css/dist'));
+    .pipe(pixrem())
+    .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', ['css']);
+gulp.task('watch', function() {
+  gulp.watch('css/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['watch']);
