f02a8f98f914eabe2a8daffa945cd3e3b02c8711
Mike Francis <mikeffrancis@gmail.com>
2019-07-17 13:07:48 +0100

    Create keyboard shortcut hook

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -3,16 +3,35 @@ import darkmodejs from "@assortment/darkmodejs";
 
 export const THEME_LIGHT = "light";
 export const THEME_DARK = "dark";
+const KEYCODE_T = 116;
 
 export const ThemeContext = React.createContext({
   theme: THEME_LIGHT,
   toggleTheme: () => {}
 });
 
+const useKeyboard = (keyCode: number, onKeyPress: Function) => {
+  React.useEffect(() => {
+    const listener = (event: KeyboardEvent) => {
+      if (event.keyCode === keyCode) {
+        onKeyPress();
+      }
+    };
+
+    window.addEventListener("keypress", listener);
+
+    return () => window.removeEventListener("keypress", listener);
+  });
+
+  return null;
+};
+
 const ThemeProvider: React.FC = ({ children }) => {
   const [theme, setTheme] = React.useState(THEME_LIGHT);
   const toggleTheme = () =>
     setTheme(theme => (theme === THEME_DARK ? THEME_LIGHT : THEME_DARK));
+
+  useKeyboard(KEYCODE_T, toggleTheme);
 
   React.useEffect(() => {
     const dmjs = darkmodejs({
