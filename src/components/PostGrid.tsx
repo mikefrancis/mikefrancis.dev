a717f3b6be864669510c71e5e9c33f76444dee48
Mike Francis <mikeffrancis@gmail.com>
2019-07-17 12:40:25 +0100

    Fix contrast issues

--- src/components/PostGrid.tsx
+++ src/components/PostGrid.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Link } from "gatsby";
 
 import { Post } from "../types";
+import { ThemeContext, THEME_DARK } from "./ThemeProvider";
 
 interface Props {
   posts: {
@@ -9,22 +10,30 @@ interface Props {
   }[];
 }
 
-const PostGrid: React.FC<Props> = ({ posts }) => (
-  <div className="md:flex md:flex-wrap md:-mx-8">
-    {posts.map(({ node }) => (
-      <div key={node.id} className="md:w-1/3 md:px-8 mb-16">
-        <article>
-          <h3 className="text-2xl mb-3">
-            <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
-          </h3>
-          <p className="mb-3 text-gray-500 uppercase text-xs tracking-widest">
-            {node.frontmatter.date}
-          </p>
-          <p className="text-sm">{node.frontmatter.description}</p>
-        </article>
-      </div>
-    ))}
-  </div>
-);
+const PostGrid: React.FC<Props> = ({ posts }) => {
+  const { theme } = React.useContext(ThemeContext);
+
+  return (
+    <div className="md:flex md:flex-wrap md:-mx-8">
+      {posts.map(({ node }) => (
+        <div key={node.id} className="md:w-1/3 md:px-8 mb-16">
+          <article>
+            <h3 className="text-2xl mb-3">
+              <Link to={node.fields.slug}>{node.frontmatter.title}</Link>
+            </h3>
+            <p
+              className={`mb-3 uppercase text-xs tracking-widest ${
+                theme === THEME_DARK ? "text-gray-500" : "text-gray-700"
+              }`}
+            >
+              {node.frontmatter.date}
+            </p>
+            <p className="text-sm">{node.frontmatter.description}</p>
+          </article>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default PostGrid;
--- src/templates/post.tsx
+++ src/templates/post.tsx
@@ -1,44 +1,54 @@
 import * as React from "react";
 import { graphql } from "gatsby";
+
 import { GraphQLResponse, MarkdownQuery, Post } from "../types";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
+import { THEME_DARK, ThemeContext } from "../components/ThemeProvider";
 
-const Page: React.FC<GraphQLResponse<MarkdownQuery<Post>>> = ({ data }) => (
-  <>
-    <SEO
-      title={data.markdownRemark.frontmatter.title}
-      description={data.markdownRemark.frontmatter.description}
-    />
-    <Layout>
-      <article className="max-w-3xl">
-        <div className="mb-8">
-          <h1 className="text-4xl mb-8">
-            {data.markdownRemark.frontmatter.title}
-          </h1>
+const Page: React.FC<GraphQLResponse<MarkdownQuery<Post>>> = ({ data }) => {
+  const { theme } = React.useContext(ThemeContext);
 
-          <div className="flex -mx-4 mb-8 text-gray-500 text-sm tracking-widest">
-            <span className="px-4 uppercase">
-              {data.markdownRemark.frontmatter.date}
-            </span>
-            <span className="px-4 uppercase">
-              {data.markdownRemark.timeToRead} minutes
-            </span>
+  return (
+    <>
+      <SEO
+        title={data.markdownRemark.frontmatter.title}
+        description={data.markdownRemark.frontmatter.description}
+      />
+      <Layout>
+        <article className="max-w-3xl">
+          <div className="mb-8">
+            <h1 className="text-4xl mb-8">
+              {data.markdownRemark.frontmatter.title}
+            </h1>
+
+            <div
+              className={`flex -mx-4 mb-8 text-sm tracking-widest ${
+                theme === THEME_DARK ? "text-gray-500" : "text-gray-700"
+              }`}
+            >
+              <span className="px-4 uppercase">
+                {data.markdownRemark.frontmatter.date}
+              </span>
+              <span className="px-4 uppercase">
+                {data.markdownRemark.timeToRead} minutes
+              </span>
+            </div>
+
+            <p className="text-2xl font-light">
+              {data.markdownRemark.frontmatter.description}
+            </p>
           </div>
 
-          <p className="text-2xl font-light">
-            {data.markdownRemark.frontmatter.description}
-          </p>
-        </div>
-
-        <div
-          className="wysiwyg leading-relaxed mb-16"
-          dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
-        />
-      </article>
-    </Layout>
-  </>
-);
+          <div
+            className="wysiwyg leading-relaxed mb-16"
+            dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+          />
+        </article>
+      </Layout>
+    </>
+  );
+};
 
 export const query = graphql`
   query($path: String!) {
