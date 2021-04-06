cc44dfb18016f7b011614c9a3e346e52e293491e
Mike Francis <mikeffrancis@gmail.com>
2021-04-06 17:58:05 +0100

    Update dependencies (#54)

--- postcss.config.js
+++ postcss.config.js
@@ -3,14 +3,5 @@ module.exports = {
     'tailwindcss',
     'autoprefixer',
     'postcss-nested',
-    process.env.NODE_ENV === 'production'
-      ? [
-          '@fullhuman/postcss-purgecss',
-          {
-            content: ['./src/**/*.{js,jsx,ts,tsx}'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-          },
-        ]
-      : undefined,
-  ].filter(Boolean),
+  ],
 };
