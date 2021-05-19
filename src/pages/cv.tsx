d10b001a97028eace4bfd33dd84bd52f240ed309
Mike Francis <mikeffrancis@gmail.com>
2021-05-19 12:19:21 +0100

    Updated CV (#57)

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -32,10 +32,10 @@ const CV: React.FC = () => {
               Mike Francis
             </h1>
             <p className="text-lg">
-              Software Engineer & UI Designer based in London, UK. Tasked with
-              bootstrapping and leading teams to deliver fast prototypes and
-              scalable solutions, while mentoring and running sprints along the
-              way.
+              Technical Leader, Software Engineer & UI Designer based in London,
+              UK. Tasked with bootstrapping and leading teams to deliver fast
+              prototypes and scalable solutions, while mentoring and running
+              sprints along the way.
             </p>
           </header>
           <main>
@@ -43,51 +43,123 @@ const CV: React.FC = () => {
               <h2 className="text-2xl mb-4">Selected Skills</h2>
               <ul className="list-disc px-8">
                 <li>
-                  JavaScript/Node.js (TypeScript, React [Native], Redux, Mocha,
-                  Koa, Webpack)
+                  JavaScript/Node.js (TypeScript, React [Native], Redux, Jest,
+                  Express, Webpack)
                 </li>
                 <li>PHP 7+ (Composer, Symfony, Laravel, PHPUnit, PSRs)</li>
-                <li>
-                  HTML5/CSS3 (Sass/LESS, OOCSS, SMACSS, BEM, Atomic CSS,
-                  CSS-in-JS)
-                </li>
-                <li>Agile Methodologies (Scrum/Kanban)</li>
+                <li>HTML5/CSS3 (Sass/LESS, BEM, Atomic CSS, CSS-in-JS)</li>
                 <li>Docker/Kubernetes, Vagrant, Chef, AWS, GCP, Heroku</li>
                 <li>Git (Gitflow, Pull Requests, Code Reviews, CI)</li>
                 <li>GraphQL/REST APIs</li>
+                <li>Agile Methodologies (Scrum/Kanban)</li>
               </ul>
             </section>
             <section className="mb-8">
               <h2 className="text-2xl mb-4">Professional Employment</h2>
+              <section>
+                <div className="flex items-center mb-2">
+                  <h3 className="text-lg font-bold mr-4">Bulb</h3>
+                  <p className="text-sm text-gray-700">
+                    London, UK • December 2018 - Present
+                  </p>
+                </div>
+                <div className="pl-8">
+                  <article className="mb-4">
+                    <div className="flex items-center mb-2">
+                      <h4 className="font-bold mr-4">Engineering Manager</h4>
+                      <p className="text-sm text-gray-700">
+                        January 2021 - Present
+                      </p>
+                    </div>
+                    <p>
+                      Managing and scaling 2 product engineering teams,
+                      currently consisting of 9 direct reports. Focussing mainly
+                      on career development & coaching, hiring, onboarding,
+                      regular feedback, continuous improvement and operational
+                      standards.
+                    </p>
+                  </article>
+                  <article className="mb-4">
+                    <div className="flex items-center mb-2">
+                      <h4 className="font-bold mr-4">Technical Lead</h4>
+                      <p className="text-sm text-gray-700">
+                        April 2020 - January 2021
+                      </p>
+                    </div>
+                    <p>
+                      Research, design and implementation of technical
+                      solutions. Guiding the quality, correctness, security and
+                      observability of systems, services and data in my team's
+                      business domain. Mentoring and coaching of software
+                      engineers and acting as a DRI for technology related
+                      projects driving continuous improvement.
+                    </p>
+                  </article>
+                  <article className="mb-4">
+                    <div className="flex items-center mb-2">
+                      <h4 className="font-bold mr-4">
+                        Senior Software Engineer
+                      </h4>
+                      <p className="text-sm text-gray-700">
+                        December 2018 - April 2020
+                      </p>
+                    </div>
+                    <p>
+                      Planning and delivering team and organisation-wide
+                      projects, implementing technical strategy and improvements
+                      while mentoring more junior members of the team. Main
+                      focusses were internal (customer service) tooling and
+                      productivity enablers, operational processes and
+                      implementing security initiatives.
+                    </p>
+                  </article>
+                </div>
+              </section>
+              <section>
+                <div className="flex items-center mb-2">
+                  <h3 className="font-bold text-lg mr-4">Pod Point</h3>
+                  <p className="text-gray-700 text-sm">
+                    London, UK • June 2015 - November 2018
+                  </p>
+                </div>
+                <div className="pl-8">
+                  <article className="mb-4">
+                    <div className="flex items-center mb-2">
+                      <h4 className="font-bold mr-4">
+                        Senior Software Engineer
+                      </h4>
+                      <p className="text-gray-700 text-sm">
+                        June 2015 - December 2018
+                      </p>
+                    </div>
+                    <p>
+                      Developing, scaling and mentoring in an agile environment
+                      on various internal and external APIs, Microservices and
+                      Native/Web Applications for a range of stakeholders.
+                    </p>
+                  </article>
+                  <article className="mb-4">
+                    <div className="flex items-center mb-2">
+                      <h4 className="font-bold mr-4">Software Engineer</h4>
+                      <p className="text-gray-700 text-sm">
+                        June 2015 - May 2017
+                      </p>
+                    </div>
+                    <p>
+                      Developing, scaling and mentoring in an agile environment
+                      on various internal and external APIs, Microservices and
+                      Native/Web Applications for a range of stakeholders.
+                    </p>
+                  </article>
+                </div>
+              </section>
               <article className="mb-4">
-                <h3 className="font-bold">Technical Lead - Bulb</h3>
-                <h4 className="text-gray-700 mb-4">
-                  London, UK • December 2018 - Present
-                </h4>
-                <p>
-                  Bootstrapping, developing and managing engineering teams
-                  across the organisation including customer service tooling,
-                  operations and vulnerable customer services.
-                </p>
-              </article>
-              <article className="mb-4">
-                <h3 className="font-bold">
-                  Senior Software Engineer - Pod Point
-                </h3>
-                <h4 className="text-gray-700 mb-4">
-                  London, UK • June 2015 - December 2018
-                </h4>
-                <p>
-                  Developing, scaling and mentoring in an agile environment on
-                  various internal and external APIs, Microservices and
-                  Native/Web Applications for a range of stakeholders.
-                </p>
-              </article>
-              <article className="mb-4">
-                <h3 className="font-bold">Lead Developer - Benchmark</h3>
-                <h4 className="text-gray-700 mb-4">
-                  London, UK • September 2013 - June 2015
-                </h4>
+                <div className="flex items-center mb-2">
+                  <h3 className="font-bold mr-4">Benchmark - Lead Developer</h3>
+                  <p className="text-gray-700 text-sm">
+                    London, UK • September 2013 - June 2015
+                  </p>
+                </div>
                 <p>
                   Development Studio Manager, introducing new workflows,
                   teaching new and existing developers new tools, frameworks and
@@ -96,10 +168,12 @@ const CV: React.FC = () => {
                 </p>
               </article>
               <article className="mb-4">
-                <h3 className="font-bold">Lead Developer - ilk</h3>
-                <h4 className="text-gray-700 mb-4">
-                  Leeds, UK • June 2008 - September 2013
-                </h4>
+                <div className="flex items-center mb-2">
+                  <h3 className="font-bold mr-4">ilk - Web Developer</h3>
+                  <p className="text-gray-700 text-sm">
+                    Leeds, UK • June 2008 - September 2013
+                  </p>
+                </div>
                 <p>
                   Also Digital Studio Manager, responsible for planning,
                   creating and maintaining interactive online and offline
@@ -108,10 +182,14 @@ const CV: React.FC = () => {
                 </p>
               </article>
               <article className="mb-4">
-                <h3 className="font-bold">Developer - Code Blue Group</h3>
-                <h4 className="text-gray-700 mb-4">
-                  Huddersfield, UK • September 2007 – June 2008
-                </h4>
+                <div className="flex items-center mb-2">
+                  <h3 className="font-bold mr-4">
+                    Code Blue Group - Web Developer
+                  </h3>
+                  <p className="text-gray-700 text-sm">
+                    Huddersfield, UK • September 2007 – June 2008
+                  </p>
+                </div>
                 <p>
                   Worked developing and maintaining websites, eCommerce stores
                   and internal apps on a pro-rata basis while studying for my
@@ -119,10 +197,14 @@ const CV: React.FC = () => {
                 </p>
               </article>
               <article>
-                <h3 className="font-bold">Volunteer - Ministry of Stories</h3>
-                <h4 className="text-gray-700 mb-4">
-                  Remote • May 2010 - Present
-                </h4>
+                <div className="flex items-center mb-2">
+                  <h3 className="font-bold mr-4">
+                    Volunteer - Ministry of Stories
+                  </h3>
+                  <p className="text-gray-700 text-sm">
+                    Remote • May 2010 - Present
+                  </p>
+                </div>
                 <p>
                   After building the original site as part of a work brief I
                   became a volunteer for Ministry of Stories and working
