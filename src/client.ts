3f34c7e05b84e569d2c0e71e17d8c91d476c7031
Mike Francis <mikeffrancis@gmail.com>
2021-09-16 13:42:11 +0100

    Preview mode

--- src/client.ts
+++ src/client.ts
@@ -22,7 +22,7 @@ function timeToRead(rawString: string) {
   return Math.ceil(totalWords / WORDS_PER_MINUTE);
 }
 
-export const transformContentfulItem = async (
+export const transformContentfulItem = (
   item: contentful.Entry<ContentfulFields>,
 ) => ({
   ...item.fields,
@@ -30,5 +30,37 @@ export const transformContentfulItem = async (
   content: micromark(item.fields.content),
   timeToRead: timeToRead(item.fields.content),
 });
+
+export const getPostBySlug = async (slug: string) => {
+  const { items } = await client.getEntries<ContentfulFields>({
+    content_type: 'post',
+    'fields.slug': slug,
+  });
+
+  if (!items.length) {
+    throw new Error(`Cannot find slug: ${slug}`);
+  }
+
+  return transformContentfulItem(items[0]);
+};
+
+export const getPreviewPostBySlug = async (slug: string) => {
+  const client = contentful.createClient({
+    accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN as string,
+    space: process.env.CONTENTFUL_SPACE_ID as string,
+    host: 'preview.contentful.com',
+  });
+
+  const { items } = await client.getEntries<ContentfulFields>({
+    content_type: 'post',
+    'fields.slug': slug,
+  });
+
+  if (!items.length) {
+    throw new Error(`Cannot find slug: ${slug}`);
+  }
+
+  return transformContentfulItem(items[0]);
+};
 
 export default client;
