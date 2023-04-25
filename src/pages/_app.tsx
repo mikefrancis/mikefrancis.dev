4203413bef997c97b5b21ca8b0c1f18133c55e4e
Mike Francis <mikeffrancis@gmail.com>
2023-04-25 15:11:57 +0100

    Add Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppPropsType } from 'next/dist/shared/lib/utils';
+import { Analytics } from '@vercel/analytics/react';
 import * as React from 'react';
 import 'typeface-work-sans';
 
@@ -9,6 +10,7 @@ const WrappedApp = ({ Component, pageProps }: AppPropsType) => {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 };
