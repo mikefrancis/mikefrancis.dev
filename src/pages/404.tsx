a4602e6095f5062c08a629d98dedb877a140b61a
Mike Francis <mikeffrancis@gmail.com>
2023-08-13 10:29:07 +0100

    Better 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,19 @@
 import { NextPage } from 'next';
 
 import Layout from '../components/Layout';
+import PageTitle from '../components/PageTitle';
 import SEO from '../components/SEO';
 
 const PageNotFound: NextPage = () => (
   <>
     <SEO title="Page not found" />
     <Layout>
-      <div className="max-w-3xl">
-        <div className="mb-8">
-          <h1 className="text-4xl mb-8">Page not found</h1>
-
-          <p>Well this is embarrasing.</p>
+      <article className="max-w-3xl mx-auto space-y-10">
+        <div className="text-center">
+          <PageTitle>Page not found</PageTitle>
+          <p className="mt-20">Well, this is embarrasing 😳</p>
         </div>
-      </div>
+      </article>
     </Layout>
   </>
 );
