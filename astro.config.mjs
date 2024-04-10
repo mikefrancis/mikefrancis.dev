0fe6b7f5b4c0a09cab3653e999aaf1a6ccefe495
Mike Francis <mikeffrancis@gmail.com>
2024-04-10 09:23:16 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/blog/6-months-of-being-an-engineering-manager': '/blog/engineering-management',
+  },
   output: "hybrid",
   adapter: vercel({
     webAnalytics: {
