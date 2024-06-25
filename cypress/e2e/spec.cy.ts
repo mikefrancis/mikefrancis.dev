0259acee8fdafdbf798b7c5b4151d7ef1b54ca01
mikefrancis <mikeffrancis@gmail.com>
2024-06-25 10:51:32 +0100

    e2e testing (#123)

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,21 @@
+describe("Pages", () => {
+  it("homepage", () => {
+    cy.visit("http://localhost:4321");
+    cy.contains("Technical Leader");
+  });
+
+  it("blog archive", () => {
+    cy.visit("http://localhost:4321/blog");
+    cy.contains("Blog archive");
+  });
+
+  it("blog post", () => {
+    cy.visit("http://localhost:4321/blog/technical-leadership");
+    cy.contains("Technical Leadership");
+  });
+
+  it("CV", () => {
+    cy.visit("http://localhost:4321/cv");
+    cy.contains("Professional Employment");
+  });
+});
