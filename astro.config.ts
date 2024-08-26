fb747520c652ced31300eddc3d075d37d2d4073e
Mike Francis <mikeffrancis@gmail.com>
2024-08-26 21:24:18 +0100

    Seeya

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,6 @@ import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 export default defineConfig({
   output: "hybrid",
   adapter: vercel({
