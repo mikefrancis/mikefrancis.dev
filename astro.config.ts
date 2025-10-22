f514869c7e1d56319966f74de15197042f45c3ad
mxkxf <mikeffrancis@gmail.com>
2025-10-22 19:08:03 +0100

    google doc

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,13 @@
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
-export default {
+export default defineConfig({
+  redirects: {
+    "/cv":
+      "https://docs.google.com/document/d/1t88b1bcL0TBoAhhCyjOnRHk2c762w6huPU5-WHisOCc",
+  },
   output: "static",
   adapter: vercel({
     webAnalytics: {
@@ -13,4 +18,4 @@ export default {
   vite: {
     plugins: [tailwindcss()],
   },
-};
+});
