568447c832a1fba2f6f289c86d4837d0a243e833
mxkxf <mikeffrancis@gmail.com>
2025-12-19 09:46:18 +0000

    Fix stars

--- src/components/PostStars.tsx
+++ src/components/PostStars.tsx
@@ -1,9 +1,13 @@
 import { createResource } from "solid-js";
 
+const BASE_URL = import.meta.env.DEV
+  ? "http://localhost:4321"
+  : "https://www.mikefrancis.dev";
+
 const getStars = async (slug: string) =>
-  (await fetch(`/blog/${slug}.json`)).json();
+  (await fetch(`${BASE_URL}/blog/${slug}.json`)).json();
 const postStars = (slug: string) =>
-  fetch(`/blog/${slug}.json`, {
+  fetch(`${BASE_URL}/blog/${slug}.json`, {
     method: "POST",
   });
 
@@ -24,34 +28,38 @@ const PostStars = ({ slug }: Props) => {
     refetch();
   };
 
+  if (!data().stars) {
+    return null;
+  }
+
+  if (data.loading) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        class="w-4 h-4 animate-pulse"
+        aria-label="Loading"
+      >
+        <path d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z"></path>
+      </svg>
+    );
+  }
+
   return (
-    <>
-      {data.loading ? (
+    <form method="post" onSubmit={handleSubmit}>
+      <button class="font-semibold flex items-center group" type="submit">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
           fill="currentColor"
-          class="w-4 h-4 animate-pulse"
-          aria-label="Loading"
+          class="w-5 h-5 mr-2 group-hover:animate-pulse"
         >
           <path d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z"></path>
         </svg>
-      ) : (
-        <form method="post" onSubmit={handleSubmit}>
-          <button class="font-semibold flex items-center group" type="submit">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="w-5 h-5 mr-2 group-hover:animate-pulse"
-            >
-              <path d="M9.653 16.915l-.005-.003-.019-.01a20.759 20.759 0 01-1.162-.682 22.045 22.045 0 01-2.582-1.9C4.045 12.733 2 10.352 2 7.5a4.5 4.5 0 018-2.828A4.5 4.5 0 0118 7.5c0 2.852-2.044 5.233-3.885 6.82a22.049 22.049 0 01-3.744 2.582l-.019.01-.005.003h-.002a.739.739 0 01-.69.001l-.002-.001z"></path>
-            </svg>
-            {data().stars ?? ""}
-          </button>
-        </form>
-      )}
-    </>
+        {data().stars}
+      </button>
+    </form>
   );
 };
 
